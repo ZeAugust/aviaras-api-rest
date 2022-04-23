0ae7da8766b7065053d18411c412381c72a3e069
ZeAugust <joseaugustoribeiro1998@gmail.com>
2022-04-23 15:22:02 -0300

    implementacao do perfil de test

--- src/main/java/com/jose/aviaras/config/TestConfig.java
+++ src/main/java/com/jose/aviaras/config/TestConfig.java
@@ -1,5 +1,21 @@
 package com.jose.aviaras.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jose.aviaras.service.AviarasService;
+
+@Configuration
+@Profile("test")
 public class TestConfig {
 
+	@Autowired
+	private AviarasService aviarasService;
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.aviarasService.instanciaBaseDeDados();
+	}
 }
